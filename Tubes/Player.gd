5f0b8671e7dc1e138ac89c4ba638b3e88ad56303
adhitptra <91391759+adhitptra@users.noreply.github.com>
2023-03-26 21:35:38 +0700

    Add files via upload

--- Tubes/Player.gd
+++ Tubes/Player.gd
@@ -1,49 +1,74 @@
 extends KinematicBody2D
 
+signal player_entering_door_signal
+signal player_entered_door_signal
 
+export var speed = 80.0
 export var walk_speed = 4.0
-const tile = 16
+const TILE_SIZE = 16
+
+onready var ray = $RayCast2D
+onready var door_ray = $PintuRayCast2D
+
+enum PlayerState {IDLE, TURNING, WALKING}
+
+var player_state = PlayerState.IDLE
 
 var initial_position = Vector2(0,0)
 var input_direction = Vector2(0,0)
-var gerak = false
-var persen = 0.0
+var percent_moved_to_next_tile = 0.0
+var is_moving = false
+var stop_input:bool = false
 
 func _ready():
-	pass
-func _physics_process(delta):
-	if(gerak == false):
-		process_player_input()
-	elif(input_direction != Vector2.ZERO):
-		move(delta)
-	else:
-		gerak = false
-	if(get_slide_count()>0):
-			for i in get_slide_count():
-				var benda = get_slide_collision(i)
-				if(benda.collider is RigidBody2D):
-					benda.collider.apply_central_impulse(input_direction)
+	$Sprite.visible =true
 
-func process_player_input():
-	if(input_direction.y == 0):
-		input_direction.x = int(Input.is_action_pressed("kanan")) - int(Input.is_action_pressed("kiri"))
-	if(input_direction.x == 0):
-		input_direction.y = int(Input.is_action_pressed("bawah")) - int(Input.is_action_pressed("atas"))
+func _physics_process(delta):
+	#if player_state == PlayerState.TURNING or stop_input:
+		#return
 	
-	if(input_direction != Vector2.ZERO):
-		initial_position = position
-		gerak = true
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("kanan"):
+		velocity.x += 1.0
+	if Input.is_action_pressed("kiri"):
+		velocity.x -= 1.0
+	if Input.is_action_pressed("atas"):
+		velocity.y -= 1.0
+	if Input.is_action_pressed("bawah"):
+		velocity.y += 1.0
+	velocity = velocity.normalized()
+	
+	
+	if velocity == Vector2.ZERO:
+		$AnimationTree.get("parameters/playback").travel("Idle")
+		pass
+	else:
+		$AnimationTree.get("parameters/playback").travel("Walk")
+		$AnimationTree.set("parameters/Idle/blend_position",velocity)
+		$AnimationTree.set("parameters/Walk/blend_position",velocity)
+		move_and_slide(velocity*speed)
+
+func entered_door():
+	emit_signal("player_entered_door_signal")
 	
 func move(delta):
-	persen += walk_speed * delta
-	if(persen >= 1.0):
-		position = initial_position +(tile * input_direction * persen)
-		persen = 0.0
-		gerak = false
-		
+	var desired_step: Vector2 = input_direction*TILE_SIZE/2
+	ray.cast_to = desired_step
+	ray.force_raycast_update()
 	
-	
+	door_ray.cast_to = desired_step
+	door_ray.force_raycast_update()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	if door_ray.is_colliding():
+		if percent_moved_to_next_tile == 0.0:
+			emit_signal("player_entering_door_signal")
+		percent_moved_to_next_tile += walk_speed*delta
+		if percent_moved_to_next_tile >= 1.0:
+			position = initial_position + (input_direction*TILE_SIZE)
+			percent_moved_to_next_tile = 0.0
+			is_moving = false
+			stop_input = true
+			$AnimationPlayer.play("Disappear")
+			$Camera2D.clear_current()
+		else:
+			position = initial_position + (input_direction*TILE_SIZE*percent_moved_to_next_tile)
